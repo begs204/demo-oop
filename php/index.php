b015483dfd794623821819bc8a6965a1e8d7bf38
Jason Begs <begs204@gmail.com>
2012-05-09 19:43:59 +0000

    renderDemoPage works!

--- php/index.php
+++ php/index.php
@@ -5,7 +5,8 @@
 </head>
 <body>
 <?php
-renderUserPage();
+//determinePage();
+renderDemoPage();
 ?>
 </body>
 </html>
@@ -20,10 +21,21 @@ function determinePage(){
 		renderUserPage();
 	}
 	elseif(isset($_GET["page"]) && $_GET["page"] == 'demo'){
-		renderDemoPage();
+		if (isset($_GET['owner_id'])){
+			renderDemoPage();			
+		}
+		else{
+			trigger_error("No Owner Defined", E_USER_ERROR);
+		}
+
 	}
 	elseif(isset($_GET["page"]) && $_GET["page"] == 'button'){
-		renderButtonPage();
+		if (isset($_GET['demo_id'])){
+			renderDemoPage();
+		}
+		else{
+			trigger_error("No Demo Defined", E_USER_ERROR);
+		}
 	}
 	elseif(isset($_GET["page"]) && $_GET["page"] == 'buttondetail'){
 		renderButtonDetailPage();
@@ -37,19 +49,31 @@ function renderUserPage(){
 	$db = new db_connection();
 	$root = 'http://ec2-50-19-198-56.compute-1.amazonaws.com/demos/php/index.php';
 	$db_response = array();
-	$db->exec('select * from users;');
+	$db->exec('select * from users where active = 1;');
 	$db->disconnect();
 	$db_response = $db->response;
 	//print $db_response[0]['firstname'];
 
 	print '<h4> Users: </h4><div id = "content">';
 	foreach ($db_response as $row) {
-		print '<a href="'.$root.'?page=demo&user='.$row['id'].'"" >'.$row['firstname'].'  '.$row['lastname'].'</a> <br>';
+		print '<a href="'.$root.'?page=demo&owner_id='.$row['id'].'" >'.$row['firstname'].'  '.$row['lastname'].'</a> <br>';
 	}
 	print '</div>';
 }
 function renderDemoPage(){
-	return true;
+
+	$db = new db_connection();
+	$root = 'http://ec2-50-19-198-56.compute-1.amazonaws.com/demos/php/index.php';
+	$db_response = array();
+	$db->exec('select id, demo_name, site_url, dashboard_id from demo where owner_id = '.$_GET['owner_id'].';');
+	$db->disconnect();
+	$db_response = $db->response;
+
+	print '<h4> Demos: </h4><div id = "content">';
+	foreach ($db_response as $row) {
+		print '<a href="'.$root.'?page=button&owner_id='.$_GET['owner_id'].'&demo_id='.$row['id'].'" >'.$row['demo_name'].'</a>   '.$row['dashboard_id'].'  <a href="'.$row['site_url'].'">'.$row['site_url'].'</a> <br>';
+	}
+	print '</div>';
 }
 function renderButtonPage(){
 	return truel;
